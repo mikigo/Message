32f3ccc7a453b9c81a56fbd1a2952c4d156bcb98
mikigo <huangmingqiang@uniontech.com>
2022-04-16 09:47:54 +0800

    fix method

--- apps/message_form/models.py
+++ apps/message_form/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Message(models.Model):
+    name = models.CharField(max_length=20, verbose_name="姓名", primary_key=True)
+    email = models.EmailField(verbose_name="邮箱")
+    address = models.CharField(max_length=100, verbose_name="联系地址")
+    message = models.CharField(max_length=1000, verbose_name="留言信息")
+
+    class Meta:
+
+        verbose_name = "留言信息"
+        verbose_name_plural = verbose_name
+        # 定义表的名称， 如果不定义默认生成表名。
+        db_table = "message"
--- apps/message_form/views.py
+++ apps/message_form/views.py
@@ -1,7 +1,33 @@
+from apps.message_form.models import Message
 from django.shortcuts import render
-
 # Create your views here.
 
 # 返回 templates下的 html 文件。
 def message_form(request):
+    # 1、查询
+    # （1）queryset 对象，可以进行 for 循环或进行切片
+    # queryset 本身并没有进行 sql 操作
+    # all_message = Message.objects.all()
+    # sliced_query = Message.objects.all()[:1]
+    # print(all_message.query)
+    # print(sliced_query.query)
+
+    # （2）filter # 会进行数据库的查询
+    # all_message = Message.objects.filter(name="mikigo") # all_message.delete()
+    # print(all_message.query)
+    # for message in all_message:
+    #     print(message.name)  # message.delete()
+
+    #（3）get 返回一个对象，数据不存在或者有多条数据，如果数据不存在会抛异常
+    # message = Message.objects.get(name="mikigo")
+    # print(message.address)
+
+    # 2、进行数据插入操作
+    message = Message()
+    message.name = "huang"
+    message.address = "beijin"
+    message.message = "second"
+    message.email = "huang@uniontech.com"
+    # 如果 key 存在，可以更新数据
+    message.save()
     return render(request, "message_form.html")
